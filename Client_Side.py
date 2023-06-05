0afc4067a5bd12350acf126b35957d955deec174
Carlos <95941247+C-Kenway@users.noreply.github.com>
2023-06-05 09:45:02 -0600

    Update Client_Side.py

--- Client_Side.py
+++ Client_Side.py
@@ -4,17 +4,29 @@ HOST = "localhost"
 PORT = 12345
 buffer_size = 1024
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
-    client_socket.connect((HOST, PORT))
-    try:
-        while True:
-            response = client_socket.recv(buffer_size).decode().strip()
-            print(response)
-            if response == "Es tu turno":
-                choice = input("Elige un índice de tarjeta para voltear: ")
-                client_socket.send(choice.encode())
-            elif response == "\n¡Felicidades! Ha ganado el juego\n":
-                break
-    finally:
-        client_socket.close()
-        print("Conexión cerrada")
+def play_game(s):
+    while True:
+        # Recibir y decodificar los datos
+        data = s.recv(buffer_size).decode()
+
+        # Verificar si los datos no están vacíos
+        if not data:
+            print("Juego terminado.")
+            break
+
+        # Imprimir el mensaje del servidor
+        print(data)
+
+        # Verificar si es el turno del cliente para jugar
+        if "Es tu turno" in data:
+            # Solicitar al usuario que ingrese su elección y enviarla al servidor
+            choice = input("Ingresa el número de la tarjeta que deseas voltear: ")
+            s.send(choice.encode())
+    s.close()
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    # Conectarse al servidor
+    s.connect((HOST, PORT))
+
+    # Iniciar el juego
+    play_game(s)
