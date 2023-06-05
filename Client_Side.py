7c354b1780c44bdda93c9dea4e8dc913ee0bee0d
Carlos <95941247+C-Kenway@users.noreply.github.com>
2023-06-05 09:16:30 -0600

    Update Client_Side.py

--- Client_Side.py
+++ Client_Side.py
@@ -4,21 +4,17 @@ HOST = "localhost"
 PORT = 12345
 buffer_size = 1024
 
-cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-cliente.connect((HOST, PORT))
-print('Se ha establecido una conexión con el servidor remoto.')
-
-while True:
-    respuesta = cliente.recv(buffer_size).decode()  # Espera la señal de que es su turno
-    if "Es tu turno" in respuesta:
-        seleccion = int(input('\nIngrese el número de la carta que desea voltear (0-7): '))
-        cliente.send(str(seleccion).encode())
-        carta_volteada = cliente.recv(buffer_size).decode()
-        print(carta_volteada)
-        respuesta = cliente.recv(buffer_size).decode()
-        print(f"Respuesta del servidor:{respuesta}")
-        if '\n¡Felicidades! Ha ganado el juego' in respuesta:
-            break
-
-cliente.close()
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+    client_socket.connect((HOST, PORT))
+    try:
+        while True:
+            response = client_socket.recv(buffer_size).decode().strip()
+            print(response)
+            if response == "Es tu turno":
+                choice = input("Elige un índice de tarjeta para voltear: ")
+                client_socket.send(choice.encode())
+            elif response == "\n¡Felicidades! Ha ganado el juego\n":
+                break
+    finally:
+        client_socket.close()
+        print("Conexión cerrada")
